a297cdc2052c1f4d82905353a4ced8a60de75632
Joann Pan <joannlpan@gmail.com>
2020-05-02 20:53:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,12 @@ def prime?(num)
    if num / 2 == 0 || num == 0 || num == 1
      return false
    else
-
+     i = 2
+     while (i < num)
+       if (num % i) == 0
+         return true
+     end
+         
 
   end
 end
