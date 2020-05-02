73d278db5ebea61dcd3e64846223ef291f81fe30
Joann Pan <joannlpan@gmail.com>
2020-05-02 21:04:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 # Add  code here!
 def prime?(num)
-   if num <= 0 || num == 1 || (num / 2) == 0
+   if num < 0 || num == 0 || num == 1
      return false
    else
      (2...num).each do |divisor|
-       if num % divisor == 0
-       return true
+       if num % divisor != 0
+       
        end
      end
 
